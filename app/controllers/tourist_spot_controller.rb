abe12c47cad7dffbbfdf0a5f3e0114eb0227260a
JulioMoquencoGomes <mokenco2015@gmail.com>
2024-05-25 16:44:45 -0300

    refatoration

--- app/controllers/tourist_spot_controller.rb
+++ app/controllers/tourist_spot_controller.rb
@@ -16,9 +16,10 @@ class TouristSpotController < ApplicationController
             places = TouristSpot.all
             encodeds = ::Service::OpenTripMap::TouristSpot.encoding_to_utf8(places)
             encodeds.each do |encoded|
-                encoded[:translate] = ::Service::RapidApi::RapidApi.new(encoded, lang).info_translate
+                encoded[:translate][:name]          = ::Service::RapidApi::RapidApi.translate(encoded["name"], lang)
+                encoded[:translate][:description]   = ::Service::RapidApi::RapidApi.translate(encoded["descrition"], lang)
             end
-            render :json => encodeds
+            render :json => { "success": true, "response": encodeds }
         rescue
             render :json => { "success": false }
         end
--- lib/service/rapid_api/rapid_api.rb
+++ lib/service/rapid_api/rapid_api.rb
@@ -1,19 +1,14 @@
 module Service
     module RapidApi
         class RapidApi
-            def initialize(model, target = "en")
-                @model  = model
-                @target = target
-            end
-            
-            def info_translate
-                {
-                    "name" => post_request(@model["name"]),
-                    "description" => post_request(@model["description"])
-                }
+       
+            def self.translate(content, target_language = "en")
+                source_language = lang_detected(content)
+                return content if source_language == target_language
+                post_request(content, source_language, target_language)
             end
 
-            def lang_detected(content)
+            def self.lang_detected(content)
                 response = ::Service::RapidApi::Request::Detects.new(content).post
                 if response.present? && response["data"].present? && 
                                         response["data"]["detections"].present? &&
@@ -23,16 +18,15 @@ module Service
                 "en"
             end
 
-            def post_request(property)
-                source = lang_detected(property)
-                response = ::Service::RapidApi::Request::Translate.new(property, source, @target).post
+            def self.post_request(content, source_language, target_language)
+                response = ::Service::RapidApi::Request::Translate.new(content, source_language, target_language).post
                 return message_error(response) unless response.present? && response["data"].present? && 
                                         response["data"]["translations"].present? &&
                                         response["data"]["translations"][0]["translatedText"].present?
                 response["data"]["translations"][0]["translatedText"]
             end
 
-            def message_error(response)
+            def self.message_error(response)
                 return response["message"] if response["message"].present?
                 nil
             end
