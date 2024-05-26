ba9ef4240be589a49a79946c417a6c0b5f16b7c2
JulioMoquencoGomes <mokenco2015@gmail.com>
2024-05-26 14:50:05 -0300

    adjusted test, needed mock values

--- spec/requests/api/tourist_spot_spec.rb
+++ spec/requests/api/tourist_spot_spec.rb
@@ -3,15 +3,20 @@ require 'swagger_helper'
 RSpec.describe 'api/tourist_spot', type: :request do
 
   path '/api/tourist_spot' do
+
     parameter name: 'name', in: :query, type: :string, description: 'name', required: true
     parameter name: 'lang', in: :query, type: :string, description: 'lang'
 
     get('list tourist_spots') do
       response(200, 'successful') do
-        after do |example|
-          example.metadata[:response][:content] = {
+
+        let(:name) { 'tóquio' }
+        let(:lang) { 'pt-BR' }
+
+        after do |execution|
+          execution.metadata[:response][:content] = {
             'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
+              execution: JSON.parse(response.body, symbolize_names: true)
             }
           }
         end
